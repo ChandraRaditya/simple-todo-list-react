38e56d3a6c6b500d384768751c297c17885ac6d6
Chandra Ariwiraditya <74780237+ChandraRaditya@users.noreply.github.com>
2022-08-18 18:23:59 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+import { useEffect, useImperativeHandle, useState } from "react";
+
+export const users = [
+  { id: 1, name: "User One", status: true },
+  { id: 2, name: "User Two", status: undefined },
+  { id: 3, name: "User Three", status: null }
+];
+
+// Case
+// Ambil data users, simpan data menggunakan useState
+// Tampilkan data user ID, user Name, user Status dan button untuk Delete.
+// Jika status true tampilkan string "ready"
+// Jika Status bukan ready tampilkan string "not ready"
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   setData(users)
+  // }, [])
+
+  // const handleDelete = (event) => {
+  //   const newData = data.splice(event, 1);
+  //   setData(newData);
+  // };
+
+  const datas = users.map((val, id) => {
+    return (
+      <li>
+        {val.id} {val.name} {val.status === true ? "ready" : "not ready"}{" "}
+        <button>Delete</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="App">
+      <h1 className="title">Front End Test - Tebengan</h1>
+      <ul>{datas}</ul>
+    </div>
+  );
+}
